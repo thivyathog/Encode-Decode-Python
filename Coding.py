c063f0d0d1521d4bc319bbfb175f98786b293c9a
Thivya Thogesan <36693842+thivyathog@users.noreply.github.com>
2019-03-24 17:49:22 +0530

    Add files via upload

---
+++ Coding.py
@@ -0,0 +1,59 @@
+def encodeMessage(message, keycode):
+    ciphertext = ""
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for letter in message:
+
+        if letter in alphabet:
+
+            keyLetter = ((alphabet.index(letter)) + keycode) % len(alphabet)
+
+            cipherLetter = alphabet[keyLetter]
+            ciphertext = ciphertext + cipherLetter
+        else:
+            cipherLetter = letter
+            ciphertext = ciphertext + cipherLetter
+
+    return ciphertext
+
+
+def decodeMessage(message, keycode):
+    ciphertext = ""
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for letter in message:
+
+        if letter in alphabet:
+
+            keyLetter = ((alphabet.index(letter)) - keycode) % len(alphabet)
+
+            cipherLetter = alphabet[keyLetter]
+            ciphertext = ciphertext + cipherLetter
+        else:
+            cipherLetter = letter
+            ciphertext = ciphertext + cipherLetter
+
+    return ciphertext
+
+
+choice = 0
+while choice < 9:
+    choice = str(input("Press e to encode, d to decode, q to exit"))
+    try:
+        choice = str(input("Press 1 to encode, 2 to decode, 9 to exit "))
+    except ValueError:
+        print("Integer isnt allowed please Re enter.")
+        continue
+    if choice.lower == "e":
+        message = input("What is the message ")
+        keycode = int(input("Enter the key"))
+        ciphertext = encodeMessage(message, keycode)
+
+    elif choice.lower() == "d":
+        message = input("What is the message ")
+        keycode = int(input("Enter the key"))
+        ciphertext = decodeMessage(message, keycode)
+
+    elif choice.lower() == "q":
+        exit(0)
+    else:
+        print("Invalid Choice")
+        continue
